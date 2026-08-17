564612fb79b8118c1a1c4e8c4953c9d5a2d70590
fluxmarchandise-sketch <fluxmarchandise@gmail.com>
2026-08-18 00:32:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ const ASSETS = [
   "./index.html",
   "./manifest.webmanifest",
   "./sw.js",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
